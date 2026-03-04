25db4d6fb6a9c73272c2705a61ae58acd6854f71
Aisha Raji <aisraj12000@gmail.com>
2026-03-04 03:21:43 -0500

    Hopefully the final version of my assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -40,6 +40,7 @@ ls ./data/raw
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
+mkdir ./data_processed
 mkdir ./data/processed ./data/processed/server_logs 
 mkdir ./data/processed ./data/processed/user_logs 
 mkdir ./data/processed ./data/processed/event_logs
